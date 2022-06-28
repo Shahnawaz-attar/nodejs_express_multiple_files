cad7222fd539c72285d30924bf932600c73ec8cc
Shahnawazatr <Shahnawaz@skills-agency.com>
2022-06-28 17:26:03 +0530

    nodejs_express_multiple_files

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const app = express();
+   
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'uploads/');
+    },
+   
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+   
+var upload = multer({ storage: storage })
+   
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+   
+app.post('/', upload.array('multi-files'), (req, res) => {
+  res.redirect('/');
+});
+   
+app.listen(3600);
